c3783f5670e4ee31f081b987abd2d95abc9baf61
anastasiia <Nastyaantonenkoa@gmail.com>
2025-11-21 22:04:34 +0200

    fix filter

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -8,14 +8,15 @@ export const getAllNotes = async (req, res) => {
 
   const skip = (page - 1) * perPage;
   const filter = {};
-  let notesQuery = Note.find(filter);
 
   if (tag) {
-    notesQuery.where("tag").equals(tag);
+    filter.tag = tag;
   }
   if (search) {
-    filter.find({ $text: { $search: search } });
+    filter.$text = { $search: search };
   }
+
+  let notesQuery = Note.find(filter);
 
   const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
